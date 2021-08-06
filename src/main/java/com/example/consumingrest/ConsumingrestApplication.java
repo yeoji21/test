48e4035530eb3f9cc3483df2db47e1883a2850dc
여지원 <yooara0508@naver.com>
2021-08-06 17:00:54 +0900

    Consuming a RESTful Web Service

---
+++ src/main/java/com/example/consumingrest/ConsumingrestApplication.java
@@ -0,0 +1,37 @@
+package com.example.consumingrest;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import org.slf4j.Logger;
+
+@SpringBootApplication
+public class ConsumingrestApplication {
+
+	private static final Logger log =
+			LoggerFactory.getLogger(ConsumingrestApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingrestApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"https://quoters.apps.pcfone.io/api/random", Quote.class);
+			log.info(quote.toString());
+		};
+	}
+
+
+}
